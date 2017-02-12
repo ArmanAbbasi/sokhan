ca665d27dd7f79d946f46c8148e63aeef46206d2
Arman Abbasi <arman.abbasi@marksandspencer.com>
2017-02-12 22:56:33 +0000

    get text from aria labels

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -1,4 +1,75 @@
+let watched = {};
+
+const getTextFromAria = (el) => {
+    const ariaLabel = el.hasAttribute('aria-label');
+    const ariaLabeledBy = el.hasAttribute('aria-labelledby');
+    const ariaDescribedBy = el.hasAttribute('aria-describedby');
+
+    let text = '';
+
+    if (ariaLabel) {
+        text += `Label: ${el.getAttribute('aria-label')}.`;
+    } else if (ariaLabeledBy) {
+        const ariaLabeledByEl = document.getElementById('aria-labelledby');
+
+        if (ariaLabeledByEl && ariaLabeledByEl.textContent) {
+            text += `Label: ${ariaLabeledByEl.textContent}.`;
+        }
+    }
+
+    if (ariaDescribedBy) {
+        const ariaDescribedByEl = document.getElementById('aria-describedby');
+
+        if (ariaDescribedByEl && ariaDescribedByEl.textContent) {
+            text += `Description: ${ariaDescribedByEl.textContent}`;
+        }
+    }
+
+    return text;
+};
+
+const getTextFromCommonAttributes = (el) => {
+    const title = el.hasAttribute('title');
+    const placeHolder = el.hasAttribute('placeholder');
+    const id = el.hasAttribute('id');
+};
+
+const getTextFromSelectionEl = (el) => {
+    const ariaText = getTextFromAria(el);
+
+    for (let key of attributes) {
+        console.log(key);
+    }
+
+};
+
+const elementActions = {
+    SELECT: getTextFromSelectionEl
+};
+
 export default {
+    isElementHidden(el) {
+        const computedStyle = window.getComputedStyle(el);
+        return computedStyle.display === 'none' || computedStyle.visibility === 'hidden' || computedStyle.opacity === '0' || el.tagName === 'NOSCRIPT' && (el.offsetWidth === 0 || el.offsetHeight === 0);
+    },
+    isElementInViewport(el) {
+        const rect = el.getBoundingClientRect();
+
+        return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+    },
+    watch(id, el, callback) {
+        if (watched[id]) { return; }
+
+        watched[id] = {
+            lastEl: el,
+            interval: setInterval(() => {
+                if (watched[id].lastEl !== document.activeElement) {
+                    watched[id].lastEl = document.activeElement;
+                    callback.call(this, watched[id].lastEl);
+                }
+            }, 200)
+        };
+    },
     findParentLink: (target) => {
         let i = 0;
         let tmp = target;
--- src/lib/sokhan.js
+++ src/lib/sokhan.js
@@ -16,223 +16,239 @@
  */
 /* globals browser, speechSynthesis */
 
-import config from './config';
+//import config from './config';
 import helper from './helper';
-
+// import store from '../store';
+// import chromeApiLayer from '../browser/chromeApiLayer';
 
 class Sokhan {
     constructor() {
-        this.isSokhanActive = true;
-        this.foundTextTimeout = null;
-
         this.bindEvents();
     }
 
+    getTextFromElement(el) {
+        return el.textContent;
+    }
+
+    elementFocused(el) {
+        console.log(this);
+        console.log(this.getTextFromElement(el));
+    }
+
     bindEvents() {
-        chrome.storage.sync.get('active', resp => {
-            this.isSokhanActive = typeof resp.active === 'boolean' ? resp.active : true;
-        });
-
-        document.addEventListener('readystatechange', () => {
-            if (document.readyState === 'complete') {
-                this.init();
-            }
-        });
+        helper.watch('activeElement', document.activeElement, this.elementFocused.bind(this));
+        // const allElements = document.querySelectorAll('*');
+        //
+        // for (let i of allElements) {
+        //     if (!helper.isElementHidden(i) && helper.isElementInViewport(i)) {
+        //         document.addEventListener('focus', (e, i) => this.elementFocused(e, i));
+        //     }
+        // }
     }
 
-    onMouseOver(e) {
-        if (this.foundTextTimeout) {
-            clearTimeout(this.foundTextTimeout);
-        }
-
-        this.foundTextTimeout = setTimeout(() => {
-            this.analyseElementAndFindText(e.target);
-        }, 250);
-    }
-
-    handleSpecialElements(el, isOnLoad) {
-        let text = '';
-        let numItems;
-
-        if (el.tagName === 'SPAN' || el.tagName === 'DIV' || el.tagName === 'IMG') {
-            if (el.tagName !== 'IMG' || (el.tagName === 'IMG' && !el.alt)) {
-                el = helper.findParentLink(el);
-                text = el.textContent || el.title || el.getAttribute('aria-label');
-            } else if (el.tagName === 'IMG' && el.alt) {
-                text = el.alt || el.title || el.getAttribute('aria-label');
-            }
-        } else if (el.tagName === 'A') {
-            text = el.textContent || el.title || el.getAttribute('aria-label');
-        } else {
-            if (el.tagName === 'SELECT') {
-                numItems = el.children.length + ' items, current selection: ';
-            }
-            text = (numItems || '') + (el.tagName === 'SELECT' ? (el.selectedOptions[0].textContent || el.getAttribute('aria-label') || 'not defined') : (el.placeholder || el.value || el.textContent || el.title || el.getAttribute('aria-label') || ''));
-        }
-
-        let type = this.getTextForTypeOfElement(el);
-
-        if (!text && el.getAttribute('aria-labelledby')) {
-            let textEl = document.getElementById(el.getAttribute('aria-labelledby'));
-            if (textEl) {
-                text = (textEl.textContent || '').trim();
-            }
-        }
-
-        if (type) {
-            this.stopSpeech();
-            this.sokhan((isOnLoad ? 'Active field: ' : '') + (type + ': ' + text));
-        }
-    }
-
-    handleTextFields(el) {
-        const text = (helper.getAttribute(el, 'aria-label') || el.textContent || el.alt || '').trim();
-        const largeSpaces = text && text.match(/\s{2,}/g);
-        const largeSpacesSum = largeSpaces && largeSpaces.length;
-
-        if (text && text.length > 2 && largeSpacesSum < 4 && text.indexOf('<div>') === -1 && text.indexOf('<span>') === -1) {
-            this.stopSpeech();
-            this.sokhan(text);
-        }
-    }
-
-    analyseElementAndFindText(target, isOnLoad) {
-        if (helper.getAttribute(target, 'aria-hidden')) { return; }
-        this[config.elementTextMap[target.tagName] ? 'handleSpecialElements' : 'handleTextFields'].call(this, target, isOnLoad);
-    }
-
-    getTextForTypeOfElement(el) {
-        let title = config.elementTextMap[el.tagName];
-
-        return (typeof title === 'string' ? title : (() => {
-            let labelEl = document.querySelector('[for="' + el.id + '"]');
-            let text = (labelEl && labelEl.textContent) || '';
-            let checked = (el.type === 'checkbox' || el.type === 'radio') ? (el.checked ? 'checked': 'unchecked') : '';
-            let relatedText = text ? (' for ' + text) : '';
-
-            return this.config.elementTextMap[el.tagName][el.type] + checked + relatedText;
-        })());
-    }
-
-    static stopSpeech() {
-        speechSynthesis.cancel();
-    }
-
-    sokhan(text) {
-        if (!this.isSokhanActive) {
-            return;
-        }
-
-        //Bug in Chrome makes browser crash.
-        text = text.replace(/\.\w+/, (match) => match.replace('.', ' . '));
-
-        if (!config.utter || !config.utter.voice || !speechSynthesis) {
-            config.setLanguageDefaults();
-        }
-
-        config.utter.text = text;
-        speechSynthesis.speak(config.utter);
-    }
-
-    bindEventToInputField(e) {
-        this.handleSpecialElements(e.currentTarget);
-        e.preventDefault();
-        e.stopPropagation();
-    }
-
-    onKeyDown(e) {
-        let key = e.key;
-
-        if (key === 'Control') {
-            this.stopSpeech();
-            this.isSokhanActive = !this.isSokhanActive;
-            chrome.storage.sync.set({active: this.isSokhanActive});
-        } else if (key === 'Shift') {
-            this.stopSpeech();
-            speechSynthesis.speak(window.sokhanConf.utter);
-        } else {
-            this.stopSpeech();
-            this.sokhan(key);
-        }
-    }
-
-    onSelectMenuClicked(e) {
-        this.stopSpeech();
-        let target = e.currentTarget,
-            choices = target.children,
-            str = 'Choices: ';
-
-        for (let i in choices) {
-            if (choices.hasOwnProperty(i) && choices[i].textContent) {
-                str += choices[i].textContent + ', ';
-            }
-        }
-
-        this.sokhan(str);
-    }
-
-    onSelectMenuChanged(e) {
-        this.stopSpeech();
-        let el = e.currentTarget;
-        let str = 'Selection menu changed to ' + (el.selectedOptions[0].textContent || el.getAttribute('aria-label') || 'not defined');
-
-        this.sokhan(str);
-    }
-
-    init() {
-        debugger;
-        this.sokhan(document.title || document.querySelector('h1').textContent || 'unknown website title ');
-        setTimeout(() => {
-            this.analyseElementAndFindText(document.activeElement, true);
-        }, 500);
-
-        chrome.storage.onChanged.addListener((changes) => {
-            if (changes.active) {
-                this.isActive = changes.active.newValue;
-            } else if (changes.autoDetect) {
-                this.config.autoDetect = changes.autoDetect.newValue;
-            } else if (changes.gender) {
-                this.config.defaultGender = changes.gender.newValue;
-                this.config.altGender = changes.gender.oldValue;
-            } else if (changes.rate) {
-                this.config.rate = changes.rate.newValue;
-            }
-            this.config.setLanguageDefaults();
-        });
-        let inputFieldEls = document.getElementsByTagName('input'),
-            linkEls = document.getElementsByTagName('a'),
-            buttonEls = document.getElementsByTagName('button'),
-            selectFieldEls = document.getElementsByTagName('select'),
-            i;
-
-        for (i in inputFieldEls) {
-            if (inputFieldEls.hasOwnProperty(i) && inputFieldEls[i].type !== 'hidden') {
-                inputFieldEls[i].addEventListener('focus', e => this.bindEventToInputField(e));
-            }
-        }
-
-        for (i in linkEls) {
-            if (linkEls.hasOwnProperty(i) && linkEls[i].type !== 'hidden') {
-                linkEls[i].addEventListener('focus', e => this.bindEventToInputField(e));
-            }
-        }
-
-        for (i in buttonEls) {
-            if (buttonEls.hasOwnProperty(i) && buttonEls[i].type !== 'hidden') {
-                buttonEls[i].addEventListener('focus', e => this.bindEventToInputField(e));
-            }
-        }
-
-        for (i in selectFieldEls) {
-            if (selectFieldEls.hasOwnProperty(i) && selectFieldEls[i].type !== 'hidden') {
-                selectFieldEls[i].addEventListener('click', e => this.onSelectMenuClicked(e));
-                selectFieldEls[i].addEventListener('change', e => this.onSelectMenuChanged(e));
-            }
-        }
-
-        document.addEventListener('mouseover', e => this.onMouseOver(e), true);
-        document.addEventListener('keydown', e => this.onKeyDown(e));
-    }
+    // bindEvents() {
+    //     chromeApiLayer.getStorage('active', ({active}) => {
+    //         chromeApiLayer.setIcon(active);
+    //     });
+    //
+    //     document.addEventListener('readystatechange', () => {
+    //         if (document.readyState === 'complete') {
+    //             this.init();
+    //         }
+    //     });
+    // }
+    //
+    // onMouseOver(e) {
+    //     if (this.foundTextTimeout) {
+    //         clearTimeout(this.foundTextTimeout);
+    //     }
+    //
+    //     this.foundTextTimeout = setTimeout(() => {
+    //         this.analyseElementAndFindText(e.target);
+    //     }, 250);
+    // }
+    //
+    // handleSpecialElements(el, isOnLoad) {
+    //     let text = '';
+    //     let numItems;
+    //
+    //     if (el.tagName === 'SPAN' || el.tagName === 'DIV' || el.tagName === 'IMG') {
+    //         if (el.tagName !== 'IMG' || (el.tagName === 'IMG' && !el.alt)) {
+    //             el = helper.findParentLink(el);
+    //             text = el.textContent || el.title || el.getAttribute('aria-label');
+    //         } else if (el.tagName === 'IMG' && el.alt) {
+    //             text = el.alt || el.title || el.getAttribute('aria-label');
+    //         }
+    //     } else if (el.tagName === 'A') {
+    //         text = el.textContent || el.title || el.getAttribute('aria-label');
+    //     } else {
+    //         if (el.tagName === 'SELECT') {
+    //             numItems = el.children.length + ' items, current selection: ';
+    //         }
+    //         text = (numItems || '') + (el.tagName === 'SELECT' ? (el.selectedOptions[0].textContent || el.getAttribute('aria-label') || 'not defined') : (el.placeholder || el.value || el.textContent || el.title || el.getAttribute('aria-label') || ''));
+    //     }
+    //
+    //     let type = this.getTextForTypeOfElement(el);
+    //
+    //     if (!text && el.getAttribute('aria-labelledby')) {
+    //         let textEl = document.getElementById(el.getAttribute('aria-labelledby'));
+    //         if (textEl) {
+    //             text = (textEl.textContent || '').trim();
+    //         }
+    //     }
+    //
+    //     if (type) {
+    //         this.stopSpeech();
+    //         this.sokhan((isOnLoad ? 'Active field: ' : '') + (type + ': ' + text));
+    //     }
+    // }
+    //
+    // handleTextFields(el) {
+    //     const text = (helper.getAttribute(el, 'aria-label') || el.textContent || el.alt || '').trim();
+    //     const largeSpaces = text && text.match(/\s{2,}/g);
+    //     const largeSpacesSum = largeSpaces && largeSpaces.length;
+    //
+    //     if (text && text.length > 2 && largeSpacesSum < 4 && text.indexOf('<div>') === -1 && text.indexOf('<span>') === -1) {
+    //         this.stopSpeech();
+    //         this.sokhan(text);
+    //     }
+    // }
+    //
+    // analyseElementAndFindText(target, isOnLoad) {
+    //     if (helper.getAttribute(target, 'aria-hidden')) { return; }
+    //     this[config.elementTextMap[target.tagName] ? 'handleSpecialElements' : 'handleTextFields'].call(this, target, isOnLoad);
+    // }
+    //
+    // getTextForTypeOfElement(el) {
+    //     let title = config.elementTextMap[el.tagName];
+    //
+    //     return (typeof title === 'string' ? title : (() => {
+    //         let labelEl = document.querySelector('[for="' + el.id + '"]');
+    //         let text = (labelEl && labelEl.textContent) || '';
+    //         let checked = (el.type === 'checkbox' || el.type === 'radio') ? (el.checked ? 'checked': 'unchecked') : '';
+    //         let relatedText = text ? (' for ' + text) : '';
+    //
+    //         return this.config.elementTextMap[el.tagName][el.type] + checked + relatedText;
+    //     })());
+    // }
+    //
+    // static stopSpeech() {
+    //     speechSynthesis.cancel();
+    // }
+    //
+    // sokhan(text) {
+    //     if (!store.isSokhanActive()) { return; }
+    //
+    //     //Bug in Chrome makes browser crash.
+    //     text = text.replace(/\.\w+/, (match) => match.replace('.', ' . '));
+    //
+    //     if (!config.utter || !config.utter.voice || !speechSynthesis) {
+    //         config.setLanguageDefaults();
+    //     }
+    //
+    //     config.utter.text = text;
+    //     speechSynthesis.speak(config.utter);
+    // }
+    //
+    // bindEventToInputField(e) {
+    //     this.handleSpecialElements(e.currentTarget);
+    //     e.preventDefault();
+    //     e.stopPropagation();
+    // }
+    //
+    // onKeyDown(e) {
+    //     let key = e.key;
+    //
+    //     if (key === 'Control') {
+    //         this.stopSpeech();
+    //         store.toggleSokhanActive();
+    //         chrome.storage.sync.set({active: store.getSokhanActive()});
+    //     } else if (key === 'Shift') {
+    //         this.stopSpeech();
+    //         speechSynthesis.speak(window.sokhanConf.utter);
+    //     } else {
+    //         this.stopSpeech();
+    //         this.sokhan(key);
+    //     }
+    // }
+    //
+    // onSelectMenuClicked(e) {
+    //     this.stopSpeech();
+    //     let target = e.currentTarget,
+    //         choices = target.children,
+    //         str = 'Choices: ';
+    //
+    //     for (let i in choices) {
+    //         if (choices.hasOwnProperty(i) && choices[i].textContent) {
+    //             str += choices[i].textContent + ', ';
+    //         }
+    //     }
+    //
+    //     this.sokhan(str);
+    // }
+    //
+    // onSelectMenuChanged(e) {
+    //     this.stopSpeech();
+    //     let el = e.currentTarget;
+    //     let str = 'Selection menu changed to ' + (el.selectedOptions[0].textContent || el.getAttribute('aria-label') || 'not defined');
+    //
+    //     this.sokhan(str);
+    // }
+    //
+    // init() {
+    //     debugger;
+    //     this.sokhan(document.title || document.querySelector('h1').textContent || 'unknown website title ');
+    //     setTimeout(() => {
+    //         this.analyseElementAndFindText(document.activeElement, true);
+    //     }, 500);
+    //
+    //     chrome.storage.onChanged.addListener((changes) => {
+    //         if (changes.active) {
+    //             this.isActive = changes.active.newValue;
+    //         } else if (changes.autoDetect) {
+    //             this.config.autoDetect = changes.autoDetect.newValue;
+    //         } else if (changes.gender) {
+    //             this.config.defaultGender = changes.gender.newValue;
+    //             this.config.altGender = changes.gender.oldValue;
+    //         } else if (changes.rate) {
+    //             this.config.rate = changes.rate.newValue;
+    //         }
+    //         this.config.setLanguageDefaults();
+    //     });
+    //     let inputFieldEls = document.getElementsByTagName('input'),
+    //         linkEls = document.getElementsByTagName('a'),
+    //         buttonEls = document.getElementsByTagName('button'),
+    //         selectFieldEls = document.getElementsByTagName('select'),
+    //         i;
+    //
+    //     for (i in inputFieldEls) {
+    //         if (inputFieldEls.hasOwnProperty(i) && inputFieldEls[i].type !== 'hidden') {
+    //             inputFieldEls[i].addEventListener('focus', e => this.bindEventToInputField(e));
+    //         }
+    //     }
+    //
+    //     for (i in linkEls) {
+    //         if (linkEls.hasOwnProperty(i) && linkEls[i].type !== 'hidden') {
+    //             linkEls[i].addEventListener('focus', e => this.bindEventToInputField(e));
+    //         }
+    //     }
+    //
+    //     for (i in buttonEls) {
+    //         if (buttonEls.hasOwnProperty(i) && buttonEls[i].type !== 'hidden') {
+    //             buttonEls[i].addEventListener('focus', e => this.bindEventToInputField(e));
+    //         }
+    //     }
+    //
+    //     for (i in selectFieldEls) {
+    //         if (selectFieldEls.hasOwnProperty(i) && selectFieldEls[i].type !== 'hidden') {
+    //             selectFieldEls[i].addEventListener('click', e => this.onSelectMenuClicked(e));
+    //             selectFieldEls[i].addEventListener('change', e => this.onSelectMenuChanged(e));
+    //         }
+    //     }
+    //
+    //     document.addEventListener('mouseover', e => this.onMouseOver(e), true);
+    //     document.addEventListener('keydown', e => this.onKeyDown(e));
+    // }
 }
 
 new Sokhan();
